dacb846d5300c50d55a84c910d90c9e5f2160791
alingasadan@gmail.com <alingasadan@gmail.com>
2019-08-16 11:16:14 +0800

    Files modified 8/16/2019 11:16 AM

--- DCRSystem/Controllers/ITController.cs
+++ DCRSystem/Controllers/ITController.cs
@@ -539,7 +539,7 @@ namespace DCRSystem.Controllers
                             CT.EmpBadgeNo = id;
                             CT.CertificationCode = certification.Code;
                             CT.DateCertified = null;
-                            CT.DateCertified = null;
+                            CT.DateRecertified = null;
                             empModel.MyCertifications.Add(CT);
                         }
                         else
@@ -629,10 +629,64 @@ namespace DCRSystem.Controllers
             return View(mode);
         }
 
+        
         [HttpGet]
-        public ActionResult Testing()
+        public ActionResult Testing(String id, String urlBack)
         {
-            return View();
+            ViewBag.URLBack = urlBack;
+            EmployeeModel empModel = new EmployeeModel();
+            if (id != null)
+            {
+                var employee = cEE.Employees_Details.Where(emp => emp.Employee_ID == id).FirstOrDefault();
+                if (employee != null)
+                {
+                    empModel.Employee = employee;
+                    empModel.Certifications = ldcr.Certifications.OrderBy(l => l.Code).ToList();
+                    empModel.TotalCertifications = ldcr.CertificationTrackers.Where(cr => cr.EmpBadgeNo == employee.Employee_ID).OrderBy(cr => cr.CertificationCode).ToList();
+                    double SKP = (Convert.ToDouble(empModel.TotalCertifications.Count()) / Convert.ToDouble(empModel.Certifications.Count() + 2)) * (100);
+                    if (!Double.IsNaN(SKP))
+                    {
+                        ViewBag.SkillsCertifiedPercentage = Convert.ToInt32(SKP);
+                    }
+                    else
+                    {
+                        ViewBag.SkillsCertifiedPercentage = 0;
+                    }
+                    var ReCertified = ldcr.CertificationTrackers.Where(cr => cr.EmpBadgeNo == employee.Employee_ID && cr.DateRecertified != null).OrderBy(cr => cr.Id).ToList().Count();
+                    ViewBag.TotalReCertified = ReCertified;
+                    double SRKP = (Convert.ToDouble(ReCertified) / Convert.ToDouble(empModel.Certifications.Count() + 2)) * (100);
+                    if (!Double.IsNaN(SRKP))
+                    {
+                        ViewBag.SkillsReCertifiedPercentage = Convert.ToInt32(SRKP);
+                    }
+                    else
+                    {
+                        ViewBag.SkillsReCertifiedPercentage = 0;
+                    }
+
+                    System.Diagnostics.Debug.WriteLine(Convert.ToInt32(SKP));
+                    System.Diagnostics.Debug.WriteLine(Convert.ToInt32(SRKP));
+
+                    foreach (var certification in empModel.Certifications)
+                    {
+                        if (empModel.TotalCertifications.FirstOrDefault(el => el.CertificationCode == certification.Code) == null)
+                        {
+                            CertificationTracker CT = new CertificationTracker();
+                            CT.Id = 0;
+                            CT.EmpBadgeNo = id;
+                            CT.CertificationCode = certification.Code;
+                            CT.DateCertified = null;
+                            CT.DateCertified = null;
+                            empModel.MyCertifications.Add(CT);
+                        }
+                        else
+                        {
+                            empModel.MyCertifications.Add(empModel.TotalCertifications.FirstOrDefault(el => el.CertificationCode == certification.Code));
+                        }
+                    }
+                }
+            }
+            return View(empModel);
         }
     }
 }
