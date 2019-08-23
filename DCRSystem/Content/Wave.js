b03c6e30c9c34184b8bfbb766c8998230d7a5a68
alingasadan@gmail.com <alingasadan@gmail.com>
2019-08-23 10:41:37 +0800

    8/23/2019 Second Changes of the day

---
+++ DCRSystem/Content/Wave.js
@@ -0,0 +1,8 @@
+﻿var text = "I wanna Wowow!";
+for (var i in text) {
+    if (text[i] === " ") {
+        $(".wavetext").append($("<span>").html("&nbsp;"));
+    } else {
+        $(".wavetext").append($("<span>").text(text[i]));
+    }
+}
